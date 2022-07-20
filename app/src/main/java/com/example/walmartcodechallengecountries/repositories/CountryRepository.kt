9d0488b0f2166ac3828b06a07553b14d5c338982
Consultant <consultant@Admins-MacBook-Pro-3.local>
2022-07-20 11:54:01 -0400

    Another improvement on response codes

--- app/src/main/java/com/example/walmartcodechallengecountries/repositories/CountryRepository.kt
+++ app/src/main/java/com/example/walmartcodechallengecountries/repositories/CountryRepository.kt
@@ -24,17 +24,8 @@ class CountryImplementation @Inject constructor(
                         emit(ResponseHelper.Success(it))
                     }
                 } else {
-                    //Code to determine the range of the response code, it will help us no determine which message display.
-                    var rangeCode = 0
-                    if(response.code() in 300..399){
-                        rangeCode = 3
-                    }else if (response.code() in 400..499){
-                        rangeCode = 4
-                    }else if(response.code() in 500..599){
-                        rangeCode = 5
-                    }
                     //Display message by range code, it will help us to set a proper message for a range of codes.
-                    val errorMessageCode = when(rangeCode){
+                    val errorMessageCode = when(getRangeCode(response.code())){
                         4 -> "Petition not created correctly, be sure to have the last version of the app"
                         5 -> "Server broken, we are fixing it, try it later again"
                         else -> "Unknown Code error, please contact us with the next code: ${response.code()}"
@@ -45,5 +36,15 @@ class CountryImplementation @Inject constructor(
                 emit(ResponseHelper.ErrorException(e))
             }
         }
+
+    private fun getRangeCode(responseCode: Int): Int{
+        //Code to determine the range of the response code, it will help us no determine which message display.
+        return when (responseCode) {
+            in 300..399 -> 3
+            in 400..499 -> 4
+            in 500..599 -> 5
+            else -> 0
+        }
+    }
 
 }
